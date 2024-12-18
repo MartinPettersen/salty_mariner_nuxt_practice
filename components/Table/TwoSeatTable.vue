2e9c0456bd48a0b0c3069423920fa7331af54954
MartinPettersen <searchingtheskyfor@gmail.com>
2024-12-18 11:22:30 +0100

    add two seat table

---
+++ components/Table/TwoSeatTable.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const props = defineProps({
+  taken: Boolean,
+  selectedTable: String,
+  tableId: String,
+});
+
+const emit = defineEmits(["update:selectedTable"]);
+
+const handleSelection = () => {
+    if (!props.taken) {
+        emit("update:selectedTable", props.tableId);
+    }   
+}
+
+</script>
+
+<template>
+  <div
+    role="button"
+    @click="handleSelection"
+    :class="[
+      'w-6 h-6 rounded-full',
+      taken ? 'bg-[#FF6F61]' : (selectedTable == tableId) ? 'bg-[#4A90E2]' : 'bg-gray-200',
+      !taken && 'hover:cursor-pointer',
+      !taken && 'hover:bg-[#7ab8ff]',
+    ]"
+  ></div>
+</template>
--- pages/order.vue
+++ pages/order.vue
@@ -1,3 +1,131 @@
+<script lang="ts" setup>
+import firstImage from "~/assets/todd-diemer-SEFR62Upnqw-unsplash.jpg";
+import { ref, watch } from "vue";
+
+const maxDate = new Date();
+maxDate.setFullYear(maxDate.getFullYear() + 5);
+
+const selectedTable = ref("");
+const email = ref("");
+const name = ref("");
+const date = ref(new Date());
+
+const tableMap = ref({
+  fourSeatTableRowOne: [
+    {
+      id: "f1",
+      taken: false,
+      selected: false,
+    },
+    {
+      id: "f2",
+      taken: false,
+      selected: false,
+    },
+    {
+      id: "f3",
+      taken: false,
+      selected: false,
+    },
+  ],
+  fourSeatTableRowTwo: [
+    {
+      id: "f4",
+      taken: false,
+      selected: false,
+    },
+    {
+      id: "f5",
+      taken: false,
+      selected: false,
+    },
+    {
+      id: "f6",
+      taken: false,
+      selected: false,
+    },
+  ],
+  twoSeatTable: [
+    {
+      id: "t1",
+      taken: false,
+      selected: false,
+    },
+    {
+      id: "t2",
+      taken: false,
+      selected: false,
+    },
+    {
+      id: "t3",
+      taken: false,
+      selected: false,
+    },
+    {
+      id: "t4",
+      taken: false,
+      selected: false,
+    },
+    {
+      id: "t5",
+      taken: false,
+      selected: false,
+    },
+  ],
+  longTable: [
+    {
+      id: "l1",
+      taken: false,
+      selected: false,
+    },
+  ],
+});
+</script>
 <template>
-    <div>Order Page</div>
+    <ImageCard :imageUrl="firstImage" alt="bilde av insiden av restauranten">
+      <div class="flex item-end justify-center">
+        <div class="flex items-center justify-center w-[80%]">
+          <div class="flex flex-col space-y-4">
+            <div class="flex w-full items-center justify-center">
+              <h2 class="font-bold text-xl">Bestill Bord</h2>
+            </div>
+            <div class="bg-white flex w-full relative overflow-visible">
+              calendar
+            </div>
+
+            <div class="flex justify-evenly space-x-3">
+              <div class="flex space-x-1">
+                <div class="border-[1px] rounded-full border-slate-700">
+                  <TableTwoSeatTable :taken="false"
+                  tableId="showcase1"
+                  :selectedTable="selectedTable"
+                  @update:selectedTable="selectedTable = $evet"
+                  />
+                </div>
+                <p>Ledig</p>
+              </div>
+              <div class="flex space-x-1">
+                <div class="border-[1px] rounded-full border-slate-700">
+                  <TableTwoSeatTable :taken="true"
+                  tableId="showcase2"
+                  :selectedTable="selectedTable"
+                  @update:selectedTable="selectedTable = $evet"/>
+                </div>
+                <p>Opptatt</p>
+              </div>
+              <div class="flex space-x-1">
+                <div class="border-[1px] rounded-full border-slate-700">
+                  <TableTwoSeatTable :taken="false"
+                  tableId="showcase3"
+                  selectedTable="showcase3"
+                  @update:selectedTable="selectedTable = $evet"/>
+                </div>
+                <p>Valgt</p>
+              </div>
+            </div>
+            tablemap OrderTableForm
+          </div>
+        </div>
+      </div>
+    </ImageCard>
 </template>
