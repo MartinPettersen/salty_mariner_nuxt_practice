e8f1277ae86e4444c41420d96fd20a2d60cec082
MartinPettersen <searchingtheskyfor@gmail.com>
2024-12-20 09:32:04 +0100

    add toggle mobile menu

--- components/Nav/Navigation.vue
+++ components/Nav/Navigation.vue
@@ -1,26 +1,47 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+const showMenu = ref(false);
+
+const toggleMenu = () => {
+  showMenu.value = !showMenu.value;
+};
+</script>
 
 <template>
-        <nav class="">
-      <ul class="flex bg-black bg-opacity-[57%] p-4 px-8 text-white w-full flex-col md:flex-row space-x-4 space-y-2 md:space-y-0 items-center justify-center ">
-        <li>
-          <NuxtLink to="/">Hjem</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/food-menu">Meny</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/wine-menu">Bar</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/order">Bestill</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/contact">Kontakt</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">Om Oss</NuxtLink>
-        </li>
-      </ul>
-    </nav>
+  <nav :class="showMenu ? '' : 'hidden md:flex'">
+    <ul
+      class="flex bg-black bg-opacity-[57%] p-4 px-8 text-white w-full flex-col md:flex-row space-x-4 space-y-2 md:space-y-0 items-center justify-center"
+    >
+      <li>
+        <NuxtLink to="/">Hjem</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/food-menu">Meny</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/wine-menu">Bar</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/order">Bestill</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/contact">Kontakt</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">Om Oss</NuxtLink>
+      </li>
+      <li>
+        <button class="md:hidden" @click="toggleMenu">Lukk</button>
+      </li>
+    </ul>
+  </nav>
+  <button
+    @click="toggleMenu"
+    :class="[
+      'bg-black bg-opacity-[57%] p-4 px-8 text-white',
+      showMenu ? 'hidden md:flex' : '',
+    ]"
+  >
+    Meny
+  </button>
 </template>
